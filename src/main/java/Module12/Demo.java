5e6d7978c20e5e5ede065078df1cfef21b120dec
Admin <vovakhalin@ukr.net>
2024-03-06 11:59:24 +0200

    First task modul12

---
+++ src/main/java/Module12/Demo.java
@@ -0,0 +1,36 @@
+package Module12;
+
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        Date d = new Date();
+                        System.out.println(d);
+                        Thread.sleep(1000);
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        Thread.sleep(5000);
+                        System.out.println("Минуло 5 секунд");
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }).start();
+    }
+}
